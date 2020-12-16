e79255486c2713daefaf53101a0c8c977833200c
ry <zijoyrb@gamil.com>
2020-12-16 16:20:24 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,12 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index  # 追記する
   end
   
 end
